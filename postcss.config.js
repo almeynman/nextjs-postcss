cfdf76f4cf01ff51a8be2a8c681185db7ac14445
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-06-23 20:10:44 +0600

    turn off autoprefixer in postcss-cssnext

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require("postcss-cssnext")({
       features: {
+        autoprefixer: false,
         customProperties: {
           variables: {
             colorPrimary: "#51a9f2"
