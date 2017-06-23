560d435f7829f94f45badf9e1864cbd598c335cd
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-06-23 20:16:29 +0600

    add import functionality

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    require("postcss-easy-import")({ prefix: "_" }), // keep this first
     require("postcss-cssnext")({
       features: {
         autoprefixer: false,
