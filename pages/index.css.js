0db190ff532533d906c6e566dc8c6e4979c38f75
Almas Akchabayev <almas.akchabayev@gmail.com>
2017-06-23 18:58:04 +0600

    move css to external js file

---
+++ pages/index.css.js
@@ -0,0 +1,12 @@
+export default `
+  .hello {
+    font: 15px Helvetica, Arial, sans-serif;
+    background: red;
+    padding: 100px;
+    text-align: center;
+    transition: 100ms ease-in background;
+  }
+  .hello:hover {
+    background: #ccc;
+  }
+`;
